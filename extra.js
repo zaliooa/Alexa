1dc5dadfc81612dfc4d636036e7e62122c8bd4b7
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-07 11:00:15 +0530

    🤩 𝙰𝙻𝙴𝚇𝙰 𝙽𝙴𝚆 𝚄𝙿𝙳𝙰𝚃𝙴 💯

---
+++ extra.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// JSL Special Functions
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+ PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ PHONE: process.env.NUMBER === undefined ? '+917025994178' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
+};
