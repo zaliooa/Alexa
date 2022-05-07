0361a377891ed08ac544d0b033fb960ef77c01ae
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-07 12:21:08 +0530

    🤩 𝙰𝙻𝙴𝚇𝙰 𝙽𝙴𝚆 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1912,7 +1912,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '```Miss Helena```'})
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALL}
 
     }));
 }
